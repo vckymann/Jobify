ad324d35fad874544fb80f52b69d0add9afe5ac0
Vikas Singh Mann <vickymann9774@gmail.com>
2025-04-18 20:08:42 -0600

    remove invalid exports.

--- src/app/api/verifyCode/route.ts
+++ src/app/api/verifyCode/route.ts
@@ -3,7 +3,7 @@ import UserModel from "@/model/User";
 import { verifySchema } from "@/schemas/verifySchema";
 import { z } from "zod";
 
-export const EmailSchema = z.object({
+const EmailSchema = z.object({
     email: z.string().email(),
 }); 
 
